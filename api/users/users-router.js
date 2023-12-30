bcff371e4f158b861d2ead896c104f61702be4d5
mwallace98 <mattwallace140@gmail.com>
2023-12-30 14:19:00 -0500

    can update user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -32,12 +32,12 @@ router.post('/', validateUser, (req, res,next) => {
     .catch(next)
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-  console.log(req.user)
-  console.log(req.name)
+router.put('/:id', validateUserId, validateUser, (req, res,next) => {
+  User.update(req.params.id, {name: req.name})
+  .then(updatedUser => {
+    res.json(updatedUser)
+  })
+  .catch(next)
 });
 
 router.delete('/:id',validateUserId ,(req, res) => {
